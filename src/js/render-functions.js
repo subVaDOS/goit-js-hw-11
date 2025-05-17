662697cabb4b24368149a3ffe4122861c9bddac1
Manokha Vadim <manokha.vadim@gmail.com>
2025-05-17 22:22:06 +0300

    modified js

---
+++ src/js/render-functions.js
@@ -0,0 +1,71 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 200,
+});
+
+export function createGallery(images, galleryElem) {
+  const markup = images
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        type,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) =>
+        `<li class="gallery-item js-gallery-item">
+  <a class="gallery-link js-gallery-link" href="${largeImageURL}">
+    <div class="image-wrapper">
+      <img
+        class="gallery-image js-gallery-image"
+        src="${webformatURL}"
+        data-source="${type}"
+        alt="${tags}"
+      />
+    </div>
+  
+  <div>
+    <ul class="wrapper-box">
+      <li class="list-item">
+        <p class="value-title">Likes</p>
+        <p class="label" data-likes>${likes}</p>
+      </li>
+      <li class="list-item">
+        <p class="value-title">Views</p>
+        <p class="label" data-views>${views}</p>
+      </li>
+      <li class="list-item">
+        <p class="value-title">Comments</p>
+        <p class="label" data-comments>${comments}</p>
+      </li>
+      <li class="list-item">
+        <p class="value-title">Downloads</p>
+        <p class="label" data-downloads>${downloads}</p>
+      </li>
+    </ul>
+  </div>
+  </a>
+</li>`
+    )
+    .join('');
+  galleryElem.insertAdjacentHTML('beforeend', markup);
+  lightbox.refresh();
+}
+
+export function clearGallery(galleryElem) {
+  galleryElem.innerHTML = '';
+}
+
+export function showLoader(loaderElem) {
+  loaderElem.classList.remove('is-hidden');
+}
+
+export function hideLoader(loaderElem) {
+  loaderElem.classList.add('is-hidden');
+}
